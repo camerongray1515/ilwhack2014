6fb5e5f06336c6562e47661bc87caa2a18db8fce
Allan Brown <s1224425@mitford.inf.ed.ac.uk>
2014-02-18 14:58:11 +0000

    Tested the csv functions. Now being returned as a dictionary.

--- twitter/simd.py
+++ twitter/simd.py
@@ -4,32 +4,56 @@ class Data():
 
     def accessData(self):
         with open("access.csv","rb") as csv_file:
-          return csv.reader(csv_file)
+          d = DictReader(csv_file)
+
+          for row in d:
+            print row
 
     def crimeData(self):
         with open("crime.csv","rb") as csv_file:
-          return csv.reader(csv_file)
+          d = DictReader(csv_file)
+
+          for row in d:
+            print row
 
     def educationData(self):
         with open("education.csv","rb") as csv_file:
-          return csv.reader(csv_file)
+          d = DictReader(csv_file)
+
+          for row in d:
+            print row
 
     def employmentData(self):
         with open("employment.csv","rb") as csv_file:
-          return csv.reader(csv_file)
+          d = DictReader(csv_file)
+
+          for row in d:
+            print row
 
     def healthData(self):
         with open("health.csv","rb") as csv_file:
-          return csv.reader(csv_file)
+          d = DictReader(csv_file)
+
+          for row in d:
+            print row
 
     def housingData(self):
         with open("housing.csv","rb") as csv_file:
-          return csv.reader(csv_file)
+          d = DictReader(csv_file)
+
+          for row in d:
+            print row
 
     def incomeData(self):
         with open("income.csv","rb") as csv_file:
-          return csv.reader(csv_file)
+          d = DictReader(csv_file)
+
+          for row in d:
+            print row
 
     def overallData(self):
         with open("overall.csv","rb") as csv_file:
-          return csv.reader(csv_file)
+          d = DictReader(csv_file)
+
+          for row in d:
+            print row
