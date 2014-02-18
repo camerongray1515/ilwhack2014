69e293e8d1e0d17eff87414ca95d7f3fa0039287
Allan Brown <s1224425@mitford.inf.ed.ac.uk>
2014-02-18 15:03:06 +0000

    CSV files were being returned as iterator of dictionaries rather than a list. Now being returned as a list of dictionaries.

--- twitter/simd.py
+++ twitter/simd.py
@@ -5,55 +5,39 @@ class Data():
     def accessData(self):
         with open("access.csv","rb") as csv_file:
           d = DictReader(csv_file)
-
-          for row in d:
-            print row
+          return list(d)
 
     def crimeData(self):
         with open("crime.csv","rb") as csv_file:
           d = DictReader(csv_file)
-
-          for row in d:
-            print row
+          return list(d)
 
     def educationData(self):
         with open("education.csv","rb") as csv_file:
           d = DictReader(csv_file)
-
-          for row in d:
-            print row
+          return list(d)
 
     def employmentData(self):
         with open("employment.csv","rb") as csv_file:
           d = DictReader(csv_file)
-
-          for row in d:
-            print row
+          return list(d)
 
     def healthData(self):
         with open("health.csv","rb") as csv_file:
           d = DictReader(csv_file)
-
-          for row in d:
-            print row
+          return list(d)
 
     def housingData(self):
         with open("housing.csv","rb") as csv_file:
           d = DictReader(csv_file)
-
-          for row in d:
-            print row
+          return list(d)
 
     def incomeData(self):
         with open("income.csv","rb") as csv_file:
           d = DictReader(csv_file)
-
-          for row in d:
-            print row
+          return list(d)
 
     def overallData(self):
         with open("overall.csv","rb") as csv_file:
           d = DictReader(csv_file)
-
-          for row in d:
-            print row
+          return list(d)
