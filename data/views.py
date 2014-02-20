d4406c5f1e951ba1a4679df2d61e58f2dab70f6c
Cameron Gray <camerong@barra.flat.camerongray.me>
2014-02-20 20:20:37 +0000

    Tweets without an *accurate* GPS location are no longer returned to be displayed on the map

--- data/views.py
+++ data/views.py
@@ -14,6 +14,13 @@ def get_tweet_meta(request):
         'features': []
     }
     for x in raw_data:
+        # Check the number of decimal places to see if we want the tweet or not
+        num_decimal_places_latitude = len(str(x.tweet.latitude).split('.')[1])
+        num_decimal_places_longitude = len(str(x.tweet.longitude).split('.')[1])
+
+        if num_decimal_places_latitude < 7 or num_decimal_places_longitude < 7:
+            continue
+
         geo_json_tweet = {
             'type': 'Feature',
             'properties': {
@@ -23,12 +30,16 @@ def get_tweet_meta(request):
             'geometry': {
                 'type': 'Point',
                 'coordinates': [x.tweet.longitude, x.tweet.latitude]
+            },
+            'jobby': {
+                'latlen': num_decimal_places_latitude,
+                'lonlen': num_decimal_places_longitude
             }
         }
 
         geo_json_tweets['features'].append(geo_json_tweet)
 
-    return HttpResponse(json.dumps(geo_json_tweets, use_decimal=True), mimetype='application/json')
+    return HttpResponse(json.dumps(geo_json_tweets, use_decimal=True), content_type='application/json')
 
 
 def get_tweet(request, tweet_id):
@@ -41,4 +52,4 @@ def get_tweet(request, tweet_id):
         'timestamp': tweet.timestamp.strftime("%Y-%m-%d %H:%M:%S")
     }
 
-    return HttpResponse(json.dumps(tweet_dict), mimetype='application/json')
+    return HttpResponse(json.dumps(tweet_dict), content_type='application/json')
